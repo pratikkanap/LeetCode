f74668c3a018f20362f473ddcce41ab690028669
KANAP PRATIK <122291908+pratikkanap@users.noreply.github.com>
2026-05-10 14:38:35 +0530

    Time: 4 ms (100%), Space: 46.1 MB (34.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        
+            int org = x;
+            int rev = 0;
+            while(x!=0){
+                int d = x % 10;
+                rev = rev *  10 + d;
+                x = x/10;
+            }
+        
+        return org == rev;
+    }
+}
